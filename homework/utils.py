31a951c68b2af939e441546add1e5973ac6e51a7
William <whyland888@gmail.com>
2023-07-25 15:02:44 -0500

    filled in Dataset class

--- homework/utils.py
+++ homework/utils.py
@@ -1,7 +1,8 @@
 from PIL import Image
-
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
+import pandas as pd
+import os
 
 LABEL_NAMES = ['background', 'kart', 'pickup', 'nitro', 'bomb', 'projectile']
 
@@ -11,26 +12,27 @@ class SuperTuxDataset(Dataset):
         """
         Your code here
         Hint: Use the python csv library to parse labels.csv
-
         WARNING: Do not perform data normalization here. 
         """
-        raise NotImplementedError('SuperTuxDataset.__init__')
+        self.path = dataset_path
+        self.image_files = [file for file in os.listdir(dataset_path) if file.endswith(".jpg")]
+        self.labels = pd.read_csv(os.path.join(dataset_path, 'labels.csv'))['label'].tolist()
 
     def __len__(self):
-        """
-        Your code here
-        """
-        raise NotImplementedError('SuperTuxDataset.__len__')
+        return len(self.image_files)
 
     def __getitem__(self, idx):
         """
         Your code here
         return a tuple: img, label
         """
-        raise NotImplementedError('SuperTuxDataset.__getitem__')
+        image_path = os.path.join(self.path, self.image_files[idx])
+        image = Image.open(image_path).convert("RGB")
+        label = self.labels[idx]
+        return image, label
 
 
-def load_data(dataset_path, num_workers=multiprocessing.cpu_count(), batch_size=128):
+def load_data(dataset_path, num_workers=24, batch_size=128):
     dataset = SuperTuxDataset(dataset_path)
     return DataLoader(dataset, num_workers=num_workers, batch_size=batch_size, shuffle=True, drop_last=False)
 
@@ -38,3 +40,8 @@ def load_data(dataset_path, num_workers=multiprocessing.cpu_count(), batch_size=
 def accuracy(outputs, labels):
     outputs_idx = outputs.max(1)[1].type_as(labels)
     return outputs_idx.eq(labels).float().mean()
+
+
+PATH = r"C:\Users\Will\OneDrive\Desktop\State Farm\UT Austin Deep Learning\homework1\data\train"
+ds = SuperTuxDataset(PATH)
+
