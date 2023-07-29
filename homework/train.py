936ebb067f3ed2f6093c8682e24b230fb9abafc3
William <whyland888@gmail.com>
2023-07-29 14:03:17 -0500

    trained MLP model

--- homework/train.py
+++ homework/train.py
@@ -2,20 +2,26 @@ from models import ClassificationLoss, model_factory, save_model
 from utils import accuracy, load_data
 import torch.optim as optim
 import torch
+#import torch_directml
 
 
 def train(args):
 
+    # Set device to GPU
+    #device = torch_directml.device()
+
     # Hyperparamaters
     batch_size = 64
     learning_rate = .001
-    num_epochs = 10
+    num_epochs = 5
 
     # Data loading
-    train_loader = load_data(r"C:\Users\Will\OneDrive\Desktop\State Farm\UT Austin Deep Learning\homework1\data\train")
-    valid_loader = load_data(r"C:\Users\Will\OneDrive\Desktop\State Farm\UT Austin Deep Learning\homework1\data\valid")
+    train_loader = load_data(r"C:\Users\Will\OneDrive\Desktop\State Farm\UT Austin Deep Learning\homework1\data\train",
+                             batch_size=batch_size)
+    valid_loader = load_data(r"C:\Users\Will\OneDrive\Desktop\State Farm\UT Austin Deep Learning\homework1\data\valid",
+                             batch_size=batch_size)
 
-    model = model_factory[args.model]()
+    model = model_factory[args.model]() #.to(device)
     criterion = ClassificationLoss()
     optimizer = optim.SGD(model.parameters(), lr=learning_rate)
 
@@ -23,6 +29,7 @@ def train(args):
     for epoch in range(num_epochs):
         train_loss = 0.0
         for i, (images, labels) in enumerate(train_loader):
+            #images, labels = images.to(device), labels.to(device)
             optimizer.zero_grad()
             outputs = model(images)
             loss = criterion(outputs, labels)
@@ -30,13 +37,14 @@ def train(args):
             optimizer.step()
 
             train_loss += loss.item()
-            print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {train_loss / len(train_loader):.4f}")
+        print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {train_loss / len(train_loader):.4f}")
 
         # Validation
         model.eval()
         val_loss = 0.0
         with torch.no_grad():
             for images, labels in valid_loader:
+                #images, labels = images.to(device), labels.to(device)
                 outputs = model(images)
                 val_loss += criterion(outputs, labels).item()
 
